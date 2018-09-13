da8d6b10fc260ae89dbc24cd9d6843579558c48c
github username <github email address>
2018-09-13 15:07:30 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -122,139 +122,223 @@ team_hash
 end
 
 def num_points_scored(player)
-game_hash[:home][:players].collect do |ke,va|
-if ke == player
-return va[:points]
+
+game_hash.each do |side, values|
+  values[:players].collect do |ke,va|
+    if ke == player
+      return va[:points]
+    end
+  end
 end
 
-end
-game_hash[:away][:players].collect do |k,v|
-if k == player
-return v[:points]
+
+# game_hash[:home][:players].collect do |ke,va|
+# if ke == player
+# return va[:points]
+#
+# end
+#
+# end
+#
+#
+# game_hash[:away][:players].collect do |k,v|
+# if k == player
+# return v[:points]
+# end
+#
+# end
 end
 
-end
-end
+
 
 def shoe_size(player)
-  game_hash[:home][:players].collect do |ke,va|
-  if ke == player
-  return va[:shoe]
-  end
+game_hash.each do |side, value|
+  value[:players].collect do |names,stats|
+    if names == player
+    return stats[:shoe]
+end
+end
+end
+end
+  # game_hash[:home][:players].collect do |ke,va|
+  # if ke == player
+  # return va[:shoe]
+  # end
+  # end
+  #
+  #
+  # game_hash[:away][:players].collect do |k,v|
+  # if k == player
+  # return v[:shoe]
+  # end
+  #
+  # end
+  # end
 
-  end
-  game_hash[:away][:players].collect do |k,v|
-  if k == player
-  return v[:shoe]
-  end
-
-  end
-  end
 
   def team_colors(team)
-game_hash[:home].each do |ke,va|
-  if va == team
-return game_hash[:home][:colors]
-  end
-
-
-  end
-  game_hash[:away].each do |k,v|
-  if v == team
-
-  end
-  return game_hash[:away][:colors]
-
+  game_hash.each do |side, value|
+  if value[:team_name] == team
+  return value[:colors]
   end
   end
+  end
+#   def team_colors(team)
+# game_hash[:home].each do |ke,va|
+#   if va == team
+# return game_hash[:home][:colors]
+#   end
+#   end
+#
+#
+#   game_hash[:away].each do |k,v|
+#   if v == team
+#
+#   end
+#   return game_hash[:away][:colors]
+#   end
+#   end
 
 
 
   def team_names
- game_hash[:home].each do |ke,va|
- team1 = va
- game_hash[:away].each do |k,v|
-   team2 =v
-   teams = []
-   teams.push(team1,team2)
-   return teams
-
- end
- end
-   end
-
-   def player_numbers(team_name)
     new_array = []
-     game_hash[:home].each do |ke,va|
-     if va == team_name
-     puts va
-     game_hash[:home][:players].collect do |ke,va|
-      new_array.push (va[:number])
-
-     end
-     return new_array
-     end
-     end
-     game_hash[:away].each do |ke,va|
-     if va == team_name
-     puts va
-     game_hash[:away][:players].collect do |ke,va|
-      new_array.push (va[:number])
-
-     end
-     return new_array
-     end
-     end
+    game_hash.each do |k,v|
+    new_array.push(v[:team_name])
+    end
+new_array
 end
 
-def player_stats(name)
-  game_hash[:home][:players].collect do |ke,va|
-  if ke == name
-  return va
+
+
+team_colors("Charlotte Hornets")
+
+
+def player_numbers(team_named)
+jersey_numbers = []
+
+game_hash.each do |side, value|
+if value[:team_name] == team_named
+value[:players].each do |names, stats|
+jersey_numbers.push(stats[:number])
+
+end
+return jersey_numbers
+end
+end
+end
+player_numbers("Charlotte Hornets")
+#    def player_numbers(team_name)
+#     new_array = []
+#      game_hash[:home].each do |ke,va|
+#      if va == team_name
+#      puts va
+#      game_hash[:home][:players].collect do |ke,va|
+#       new_array.push (va[:number])
+#
+#      end
+#      return new_array
+#      end
+#      end
+#      game_hash[:away].each do |ke,va|
+#      if va == team_name
+#      puts va
+#      game_hash[:away][:players].collect do |ke,va|
+#       new_array.push (va[:number])
+#
+#      end
+#      return new_array
+#      end
+#      end
+# end
+
+
+
+def player_stats(player_name)
+  game_hash.each do |side, value|
+  value[:players].each do |name, stats|
+
+  if name == player_name
+  return stats
   end
 
   end
-  game_hash[:away][:players].collect do |k,v|
-  if k == name
-  return v
-  end
-
   end
 end
+# def player_stats(name)
+#   game_hash[:home][:players].collect do |ke,va|
+#   if ke == name
+#   return va
+#   end
+#
+#   end
+#   game_hash[:away][:players].collect do |k,v|
+#   if k == name
+#   return v
+#   end
+#
+#   end
+# end
+
+
+
 
 
 def big_shoe_rebounds
-     new_array = []
-     size = 0
-     rebounds = 0
 
-   game_hash[:home][:players].collect do |k,v|
+new_array = []
+size = 0
+rebounds = 0
 
-  if v[:shoe] > size
-  size = v[:shoe]
-  rebounds = v[:rebounds]
-  new_array[0] = k
-  new_array[1] = v[:shoe]
-  new_array[2] = v[:rebounds]
+  game_hash.each do |side, value|
+  value[:players].each do |name, stats|
+if stats[:shoe] > size
+  size = stats[:shoe]
+  rebounds = stats[:rebounds]
+  new_array[0] = name
+  new_array[1] = stats[:shoe]
+  new_array[2] = stats[:rebounds]
+
+end
+
+end
+end
+ return new_array.last
 
 
-
-  return new_array.last
-else
-  game_hash[:away][:players].collect do |k,v|
-
-  if v[:shoes] > size
-  size = v[:shoe]
-  rebounds = v[:rebounds]
-  new_array[0] = k
-  new_array[1] = v[:shoe]
-  new_array[2] = v[:rebounds]
-
-  end
-
-  end
-
-  puts new_array.last
-  end
-  end
-  end
+end
+# def big_shoe_rebounds
+#      new_array = []
+#      size = 0
+#      rebounds = 0
+#
+#    game_hash[:home][:players].collect do |k,v|
+#
+#   if v[:shoe] > size
+#   size = v[:shoe]
+#   rebounds = v[:rebounds]
+#   new_array[0] = k
+#   new_array[1] = v[:shoe]
+#   new_array[2] = v[:rebounds]
+#
+#
+#
+#   return new_array.last
+# else
+#   game_hash[:away][:players].collect do |k,v|
+#
+#   if v[:shoes] > size
+#   size = v[:shoe]
+#   rebounds = v[:rebounds]
+#   new_array[0] = k
+#   new_array[1] = v[:shoe]
+#   new_array[2] = v[:rebounds]
+#
+#   end
+#
+#   end
+#
+#   puts new_array.last
+#   end
+#   end
+#   end
